d48e778d90a611038940168454f1ce994814a746
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2024-05-04 15:07:00 -0300

    Rename `file_data` to `f`

--- src/n3ds/n3ds_structures.rs
+++ src/n3ds/n3ds_structures.rs
@@ -93,33 +93,29 @@ impl SMDHIcon {
 }
 
 impl SMDHIcon {
-    pub fn from_smdh<T: Read + Seek>(
-        file_data: &mut T,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_smdh<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
         let mut smdh_magic = [0u8; 4];
-        file_data.read_exact(&mut smdh_magic)?;
+        f.read_exact(&mut smdh_magic)?;
         if "SMDH".as_bytes() != smdh_magic {
             return Err(Box::new(N3DSParsingErrorSMDHMagicNotFound));
         }
 
-        file_data.seek(SeekFrom::Current(0x24C0 - 0x04))?;
+        f.seek(SeekFrom::Current(0x24C0 - 0x04))?;
         let mut large_icon_bytes = [0u8; 0x1200];
-        file_data.read_exact(&mut large_icon_bytes)?;
+        f.read_exact(&mut large_icon_bytes)?;
         let large_icon = SMDHIcon::generate_pixbuf_from_bytes(large_icon_bytes)?;
         Ok(SMDHIcon { large_icon })
     }
 
-    pub fn from_n3dsx<T: Read + Seek>(
-        file_data: &mut T,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_n3dsx<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
         let mut n3dsx_magic = [0u8; 4];
-        file_data.read_exact(&mut n3dsx_magic)?;
+        f.read_exact(&mut n3dsx_magic)?;
         if "3DSX".as_bytes() != n3dsx_magic {
             return Err(Box::new(N3DSParsingError3DSXMagicNotFound));
         }
 
         let mut header_size = [0u8; 2];
-        file_data.read_exact(&mut header_size)?;
+        f.read_exact(&mut header_size)?;
         let header_size = u16::from_le_bytes(header_size);
         if !(header_size > 32) {
             return Err(Box::new(N3DSParsingError3DSXNoExtendedHeader {
@@ -127,42 +123,42 @@ impl SMDHIcon {
             }));
         }
 
-        file_data.seek(SeekFrom::Start(0x20))?;
+        f.seek(SeekFrom::Start(0x20))?;
         let mut smdh_offset = [0u8; 4];
-        file_data.read_exact(&mut smdh_offset)?;
+        f.read_exact(&mut smdh_offset)?;
         let smdh_offset = u32::from_le_bytes(smdh_offset);
 
         let mut smdh_size = [0u8; 4];
-        file_data.read_exact(&mut smdh_size)?;
+        f.read_exact(&mut smdh_size)?;
         let _smdh_size = u32::from_le_bytes(smdh_size);
 
-        file_data.seek(SeekFrom::Start(smdh_offset.into()))?;
-        let smdh_icon = SMDHIcon::from_smdh(file_data)?;
+        f.seek(SeekFrom::Start(smdh_offset.into()))?;
+        let smdh_icon = SMDHIcon::from_smdh(f)?;
         Ok(smdh_icon)
     }
 
-    pub fn from_cia<T: Read + Seek>(file_data: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_cia<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
         /*
          * The meta section isn't in a fixed place and is located after a bunch of sections whose
          * size can vary, therefore it's needed to at the very last fetch the other sizes and
          * take the padding into account
          */
 
-        file_data.seek(SeekFrom::Start(0x08))?;
+        f.seek(SeekFrom::Start(0x08))?;
         let mut certificate_chain_size = [0u8; 4];
-        file_data.read_exact(&mut certificate_chain_size)?;
+        f.read_exact(&mut certificate_chain_size)?;
         let certificate_chain_size = u32::from_le_bytes(certificate_chain_size);
 
         let mut ticket_size = [0u8; 4];
-        file_data.read_exact(&mut ticket_size)?;
+        f.read_exact(&mut ticket_size)?;
         let ticket_size = u32::from_le_bytes(ticket_size);
 
         let mut tmd_size = [0u8; 4];
-        file_data.read_exact(&mut tmd_size)?;
+        f.read_exact(&mut tmd_size)?;
         let tmd_size = u32::from_le_bytes(tmd_size);
 
         let mut meta_size = [0u8; 4];
-        file_data.read_exact(&mut meta_size)?;
+        f.read_exact(&mut meta_size)?;
         let meta_size = u32::from_le_bytes(meta_size);
 
         let meta_size = CIAMetaSize::try_from(meta_size)?;
@@ -176,7 +172,7 @@ impl SMDHIcon {
         };
 
         let mut content_size = [0u8; 8];
-        file_data.read_exact(&mut content_size)?;
+        f.read_exact(&mut content_size)?;
         let content_size = u64::from_le_bytes(content_size);
 
         let certificate_chain_size_with_padding = certificate_chain_size.div_ceil(0x40) * 0x40;
@@ -189,30 +185,28 @@ impl SMDHIcon {
             + u64::from(ticket_size_with_padding)
             + u64::from(tmd_size_with_padding)
             + content_size_with_padding;
-        file_data.seek(SeekFrom::Start(0x2040 + sections_offset))?;
-        let smdh_icon = SMDHIcon::from_cia_meta(file_data)?;
+        f.seek(SeekFrom::Start(0x2040 + sections_offset))?;
+        let smdh_icon = SMDHIcon::from_cia_meta(f)?;
         Ok(smdh_icon)
     }
 
-    pub fn from_cia_meta<T: Read + Seek>(
-        file_data: &mut T,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
-        file_data.seek(SeekFrom::Current(0x400))?;
-        let smdh_icon = SMDHIcon::from_smdh(file_data)?;
+    pub fn from_cia_meta<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
+        f.seek(SeekFrom::Current(0x400))?;
+        let smdh_icon = SMDHIcon::from_smdh(f)?;
         Ok(smdh_icon)
     }
 
-    pub fn from_cci<T: Read + Seek>(file_data: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
-        file_data.seek(SeekFrom::Start(0x100))?;
+    pub fn from_cci<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
+        f.seek(SeekFrom::Start(0x100))?;
         let mut cci_magic = [0u8; 4];
-        file_data.read_exact(&mut cci_magic)?;
+        f.read_exact(&mut cci_magic)?;
         if "NCSD".as_bytes() != cci_magic {
             return Err(Box::new(N3DSParsingErrorCCIMagicNotFound));
         }
 
-        file_data.seek(SeekFrom::Start(0x120))?;
+        f.seek(SeekFrom::Start(0x120))?;
         let partition_table = (0..8)
-            .map(|_| CCIPartition::from_data(file_data))
+            .map(|_| CCIPartition::from_data(f))
             .collect::<Result<Vec<_>, _>>()?;
         let Some(first_partition) = partition_table.first() else {
             return Err(Box::new(
@@ -220,49 +214,47 @@ impl SMDHIcon {
             ));
         };
 
-        file_data.seek(SeekFrom::Start(first_partition.offset().into()))?;
-        let smdh_icon = SMDHIcon::from_cxi(file_data)?;
+        f.seek(SeekFrom::Start(first_partition.offset().into()))?;
+        let smdh_icon = SMDHIcon::from_cxi(f)?;
         Ok(smdh_icon)
     }
 
-    pub fn from_cxi<T: Read + Seek>(file_data: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
-        file_data.seek(SeekFrom::Current(0x100))?;
+    pub fn from_cxi<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
+        f.seek(SeekFrom::Current(0x100))?;
         let mut cxi_magic = [0u8; 4];
-        file_data.read_exact(&mut cxi_magic)?;
+        f.read_exact(&mut cxi_magic)?;
         if "NCCH".as_bytes() != cxi_magic {
             return Err(Box::new(N3DSParsingErrorCXIMagicNotFound));
         }
 
-        file_data.seek(SeekFrom::Current(0x188 - 0x104))?;
+        f.seek(SeekFrom::Current(0x188 - 0x104))?;
         let mut flags = [0u8; 8];
-        file_data.read_exact(&mut flags)?;
+        f.read_exact(&mut flags)?;
         let flags_index_7 = flags[7];
         let is_no_crypto = (flags_index_7 & 0x4) == 0x4;
         if !is_no_crypto {
             return Err(Box::new(N3DSParsingErrorCXIFileEncrypted));
         }
 
-        file_data.seek(SeekFrom::Current(0x1A0 - 0x190))?;
+        f.seek(SeekFrom::Current(0x1A0 - 0x190))?;
         let mut exefs_offset = [0u8; 4];
-        file_data.read_exact(&mut exefs_offset)?;
+        f.read_exact(&mut exefs_offset)?;
         let exefs_offset = u32::from_le_bytes(exefs_offset); // in media units
         let exefs_offset = exefs_offset * 0x200;
 
         let mut exefs_size = [0u8; 4];
-        file_data.read_exact(&mut exefs_size)?;
+        f.read_exact(&mut exefs_size)?;
         let exefs_size = u32::from_le_bytes(exefs_size); // in media units
         let _exefs_size = exefs_size * 0x200;
 
-        file_data.seek(SeekFrom::Current(exefs_offset as i64 - 0x1A8))?;
-        let smdh_icon = SMDHIcon::from_exefs(file_data)?;
+        f.seek(SeekFrom::Current(exefs_offset as i64 - 0x1A8))?;
+        let smdh_icon = SMDHIcon::from_exefs(f)?;
         Ok(smdh_icon)
     }
 
-    pub fn from_exefs<T: Read + Seek>(
-        file_data: &mut T,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_exefs<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
         let file_headers: Vec<ExeFSFileHeader> = (0..10)
-            .map(|_| ExeFSFileHeader::from_data(file_data))
+            .map(|_| ExeFSFileHeader::from_data(f))
             .collect::<Result<Vec<_>, _>>()?
             .into_iter()
             .flatten()
@@ -272,10 +264,10 @@ impl SMDHIcon {
             return Err(Box::new(N3DSParsingErrorExeFSIconFileNotFound));
         };
 
-        file_data.seek(SeekFrom::Current(
+        f.seek(SeekFrom::Current(
             0x200 + icon_file.file_offset() as i64 - 0xA0,
         ))?;
-        let smdh_icon = SMDHIcon::from_smdh(file_data)?;
+        let smdh_icon = SMDHIcon::from_smdh(f)?;
         Ok(smdh_icon)
     }
 }
@@ -309,16 +301,14 @@ pub struct CCIPartition {
 }
 
 impl CCIPartition {
-    pub fn from_data<T: Read + Seek>(
-        file_data: &mut T,
-    ) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn from_data<T: Read + Seek>(f: &mut T) -> Result<Self, Box<dyn std::error::Error>> {
         let mut offset = [0u8; 0x4];
-        file_data.read_exact(&mut offset)?;
+        f.read_exact(&mut offset)?;
         let offset = u32::from_le_bytes(offset); //in media units
         let offset = offset * 0x200;
 
         let mut length = [0u8; 0x4];
-        file_data.read_exact(&mut length)?;
+        f.read_exact(&mut length)?;
         let length = u32::from_le_bytes(length); //in media units
         let length = length * 0x200;
 
@@ -347,13 +337,13 @@ pub struct ExeFSFileHeader {
 
 impl ExeFSFileHeader {
     pub fn from_data<T: Read + Seek>(
-        file_data: &mut T,
+        f: &mut T,
     ) -> Result<Option<Self>, Box<dyn std::error::Error>> {
         // Each header is composed of 16 bytes, if the header is empty it will be filled with zeroes
         // Therefore we can read it as a u128 and check if it's results in a zero as a small optimization
 
         let mut file_header = [0u8; 16];
-        file_data.read_exact(&mut file_header)?;
+        f.read_exact(&mut file_header)?;
 
         let is_empty = u128::from_ne_bytes(file_header);
         let is_empty = is_empty == 0;
