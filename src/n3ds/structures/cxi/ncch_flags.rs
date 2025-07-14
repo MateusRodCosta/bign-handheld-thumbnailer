ab2f035ca355634f79f1caad48119bfe75d0e2bf
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-07-14 13:59:44 -0300

    Implement bitflags usage on ncch_flags
    As suggested by Gemini

--- src/n3ds/structures/cxi/ncch_flags.rs
+++ src/n3ds/structures/cxi/ncch_flags.rs
@@ -1,3 +1,5 @@
+use bitflags::bitflags;
+
 #[derive(Debug)]
 pub struct NCCHFlags {
     _crypto_method: NCCHCryptoMethodFlags,
@@ -9,8 +11,8 @@ impl From<[u8; 8]> for NCCHFlags {
     fn from(value: [u8; 8]) -> Self {
         NCCHFlags {
             _crypto_method: NCCHCryptoMethodFlags::from(value[3]),
-            _content_type: NCCHContentTypeFlags::from(value[5]),
-            security: NCCHSecurityFlags::from(value[7]),
+            _content_type: NCCHContentTypeFlags::from_bits_truncate(value[5]),
+            security: NCCHSecurityFlags::from_bits_truncate(value[7]),
         }
     }
 }
@@ -50,60 +52,43 @@ impl From<u8> for NCCHCryptoMethodFlags {
     }
 }
 
-#[derive(Debug)]
-pub struct NCCHContentTypeFlags {
-    _data: bool,
-    _executable: bool,
-    _system_update: bool,
-    _manual: bool,
-    _child: bool,
-    _trial: bool,
-}
+bitflags! {
+    #[repr(transparent)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct NCCHContentTypeFlags: u8 {
+        const DATA = 0x01;
+        const EXECUTABLE = 0x02;
+        const SYSTEM_UPDATE = 0x04;
+        const MANUAL = 0x08;
+        const TRIAL = 0x10;
 
-impl From<u8> for NCCHContentTypeFlags {
-    fn from(value: u8) -> Self {
-        NCCHContentTypeFlags {
-            _data: (value & 0x1) != 0,
-            _executable: (value & 0x2) != 0,
-            _system_update: (value & 0x4) != 0,
-            _manual: (value & 0x8) != 0,
-            _child: (value & (0x4 | 0x8)) != 0,
-            _trial: (value & 0x10) != 0,
-        }
+        const CHILD = NCCHContentTypeFlags::SYSTEM_UPDATE.bits() | NCCHContentTypeFlags::MANUAL.bits();
     }
 }
 
 impl NCCHContentTypeFlags {
     fn _is_cfa(&self) -> bool {
-        self._data && !self._executable
+        self.contains(Self::DATA) && !self.contains(Self::EXECUTABLE)
     }
 
     fn _is_cxi(&self) -> bool {
-        self._data && self._executable
+        self.contains(Self::EXECUTABLE)
     }
 }
 
-#[derive(Debug)]
-pub struct NCCHSecurityFlags {
-    _fixed_crypto_key: bool,
-    _no_mount_rom_fs: bool,
-    no_crypto: bool,
-    _new_key_y_generator: bool,
-}
-
-impl From<u8> for NCCHSecurityFlags {
-    fn from(value: u8) -> Self {
-        NCCHSecurityFlags {
-            _fixed_crypto_key: (value & 0x1) != 0,
-            _no_mount_rom_fs: (value & 0x2) != 0,
-            no_crypto: (value & 0x4) != 0,
-            _new_key_y_generator: (value & 0x20) != 0,
-        }
+bitflags! {
+    #[repr(transparent)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct NCCHSecurityFlags: u8 {
+        const FIXED_CRYPTO_KEY = 0x1;
+        const NO_MOUNT_ROM_FS = 0x2;
+        const NO_CRYPTO = 0x4;
+        const NEW_KEY_Y_GENERATOR = 0x20;
     }
 }
 
 impl NCCHSecurityFlags {
     pub fn is_not_encrypted(&self) -> bool {
-        self.no_crypto
+        self.contains(Self::NO_CRYPTO)
     }
 }
