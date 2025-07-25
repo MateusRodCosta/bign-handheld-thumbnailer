e29b4056de8e168399ab06c08f47aae905c13bdb
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2025-07-25 14:49:25 -0300

    Simplify get_mime_type code

--- src/utils.rs
+++ src/utils.rs
@@ -6,7 +6,6 @@ use gio::{prelude::FileExt, Cancellable};
 
 use crate::error::MimeTypeDetectionError;
 
-
 pub fn get_mime_type(input: &Path) -> Result<String, MimeTypeDetectionError> {
     let file = gio::File::for_path(input);
     let attrs = gio::FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE;
@@ -14,7 +13,6 @@ pub fn get_mime_type(input: &Path) -> Result<String, MimeTypeDetectionError> {
 
     let mime_type = file_info
         .content_type()
-        .and_then(|c| gio::functions::content_type_get_mime_type(&c))
         .ok_or(MimeTypeDetectionError::MimeTypeDetectionFailure)?;
 
     Ok(mime_type.to_string())
